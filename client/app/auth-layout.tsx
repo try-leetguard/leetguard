2cd30943936cbdecbb89c420411010361f16e1c2
NguyenAnh28 <nguyenanh.tr2006@gmail.com>
2026-05-21 10:25:15 +0700

    change: migrated client code. removed duplicated wrappers by merging pages into their respected categories: auth, dashboard, marketing.

--- client/app/auth-layout.tsx
+++
@@ -1,9 +0,0 @@
-import { ReverseProtectedRoute } from "@/components/ReverseProtectedRoute";
-
-export default function AuthLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <ReverseProtectedRoute>{children}</ReverseProtectedRoute>;
-}
